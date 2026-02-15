3f245c6ae1649513d937accc7c22f63ee62fdb38
udan-insith <udaninsith23613@gmail.com>
2026-02-15 12:48:41 +0530

    final output

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const questionBtns = document.querySelectorAll(".question-btn")
+// const questionBtns = document.querySelectorAll(".question-btn")
 
 
-questionBtns.forEach(function(questionBtn) {
-    questionBtn.addEventListener('click', function(e) {
-        e.currentTarget.parentElement.parentElement.classList.toggle("show-text")
+// questionBtns.forEach(function(questionBtn) {
+//     questionBtn.addEventListener('click', function(e) {
+//         e.currentTarget.parentElement.parentElement.classList.toggle("show-text")
+//     })
+// })
+
+
+
+// New Method
+const questions = document.querySelectorAll(".question")
+
+questions.forEach(function(question) {
+    const btn = question.querySelector(".question-btn")
+    btn.addEventListener("click", function() {
+        questions.forEach(function(item) {
+            if(item !== question){
+                item.classList.remove("show-text")
+            }
+        })
+        question.classList.toggle("show-text")
     })
 })
